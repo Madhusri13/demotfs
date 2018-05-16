a3f24d21fbd37442c96900711554d3578805b10e
jegasuguna <jegasuguna.murugan@cognizant.com>
2018-05-16 11:14:51 +0530

    comment

---
+++ demojunit/src/com/junit/Add.java
@@ -0,0 +1,33 @@
+package com.junit;
+public class Add {
+    
+	int a,b,c;
+	//hello
+	public Add(int a,int b,int c)
+	{
+		this.a=a;
+		this.b=b;
+		this.c=c;
+	}
+	public int sum()
+	{
+		return a+b+c;
+		
+	}
+	
+   public int multiply()
+   {
+	   return a*b*c;  
+   }
+   
+   
+  
+	public static void main(String[] args) {
+		Add var=new Add(2,3,4);
+		System.out.println("The sum  is "+var.sum());
+		System.out.println("The multiply is "+var.multiply());
+		System.out.println("The divide value is "+var.sum()/10);
+
+	}
+
+}
